b88078356fc649091ad8165025403ba92cd5e6df
JoseEdeNeto <jose.ede.neto@gmail.com>
2019-10-21 21:12:59 -0300

    list_comprehensions

---
+++ Python 3 Programming/list_comprehensions.py
@@ -0,0 +1,9 @@
+fruits = ['apple', 'mango', 'kiwi', 'watermelon', 'pear']
+
+fruits_len = [len(item) for item in fruits]
+
+print(fruits_len)
+
+fruits_mp = fruits_mp = [fruit for fruit in fruits if (fruit[0] in ['m','p'])]
+
+print(fruits_mp)
